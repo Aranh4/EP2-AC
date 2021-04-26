eb235a3a598e53ff3f1b6b91269b19886c2dbfe6
Arthur Fonseca <81189803+arthurfonsek@users.noreply.github.com>
2021-04-26 16:33:36 -0300

    Cria baralho (1o Commit)

---
+++ CRIABARALHO.py
@@ -0,0 +1,12 @@
+#Bibliotecas
+import random
+#Código
+
+def cria_baralho():
+    espadas = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠']
+    copas = ['A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥']
+    ouros = ['A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦']
+    paus = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣']
+    baralho = espadas + ouros + paus + copas
+    random.shuffle(baralho)
+    return (baralho)
