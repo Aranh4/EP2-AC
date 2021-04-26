55d62590ecdd49cb9eeb87c07eb02a12635d5dd6
arthurfonsek <arthur.b.dafonseca@gmail.com>
2021-04-26 16:45:39 -0300

    corrigindo algumas imperfeições

--- CRIABARALHO.py
+++ CRIABARALHO.py
@@ -1,7 +1,7 @@
 #Bibliotecas
 import random
-#Código
 
+#Código
 def cria_baralho():
     espadas = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠']
     copas = ['A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥']
