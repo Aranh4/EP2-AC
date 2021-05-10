6e1b9ac2ba2cbd1c69dae7491fa3ad777d38327d
Caio <caranhatieri@gmail.com>
2021-05-10 13:44:35 -0300

    atualizacao Lista_movimentos_possiveis

--- Movimentospossiveis.py
+++ Movimentospossiveis.py
@@ -1,6 +1,6 @@
 import ExtraiNaipe
 import EXTRAIVALOR
-
+"""
 def lista_movimentos_possiveis(baralho,i):
     movimentos = []
     if i == 0:
@@ -17,3 +17,15 @@ def lista_movimentos_possiveis(baralho,i):
         return movimentos
     elif i>4:
         return movimentos
+"""
+
+def lista_movimentos_possiveis(baralho,i):
+    movimentos = []
+    if i == 0:
+        return movimentos
+    if extrai_valor(baralho[i]) == extrai_valor(baralho[i-1]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-1]):
+            movimentos.append(1)
+    if i>=3:
+        if extrai_valor(baralho[i]) == extrai_valor(baralho[i-3]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-3]):
+            movimentos.append(3)
+    return movimentos
