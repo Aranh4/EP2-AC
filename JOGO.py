92695cb15489b3bd4169c82315155392895f1e0d
arthurfonsek <arthur.b.dafonseca@gmail.com>
2021-05-11 23:23:53 -0300

    EU CORRIGI O BUG, OBRIGADO DEUS!

--- JOGO.py
+++ JOGO.py
@@ -52,12 +52,20 @@ while jogar == "S":
             while True:
                 numerovalido = False
                 while not numerovalido:
-                    numeroescolhido = int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
-                    if numeroescolhido > len(baralho) or numeroescolhido < 1:
-                        print('Número inválido, escolha outro válido. (Entre 1 a {}): '.format(len(baralho)))
-                        print('')
+                    numeroescolhido = input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho)))
+                    numeroescolhidoint = numeroescolhido.isnumeric()
+                    if numeroescolhidoint == True:
+                        numeroescolhido = int(numeroescolhidoint)
+                        if numeroescolhido > len(baralho) or numeroescolhido < 1:
+                            print('Número inválido, escolha outro válido. (Entre 1 a {}): '.format(len(baralho)))
+                            print('')
+                        else:
+                            numerovalido = True
                     else:
-                        numerovalido = True
+                        print('')
+                        print('Opção inválida, digite um número. (Entre 1 a {}): '.format(len(baralho)))
+                        print('')
+                        numerovalido = False
                 indexcarta = numeroescolhido-1
                 #VERIFICANDO SE HA MOVIMENTOS POSSIVEIS NAQUELA CARTA
                 movpos = lista_movimentos_possiveis(baralho,indexcarta)
